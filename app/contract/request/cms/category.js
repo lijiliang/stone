ff77ce26e73b44b067d6d8f91ec62b57ae64954b
Benson <sucaiweb@163.com>
2019-02-18 16:03:41 +0800

    feat: 修正栏目数据

--- app/contract/request/cms/category.js
+++ app/contract/request/cms/category.js
@@ -2,17 +2,20 @@
 
 module.exports = {
   categoryCreateRequest: {
-    title: { type: 'string', required: true, description: '标题', message: '标题不能为空' },
-    path: { type: 'string', required: true, description: '路径', message: '路径不能为空' },
-    component: { type: 'string', required: true, description: '组件', message: '组件不能为空' },
-    componentPath: { type: 'string', required: false, allowEmpty: true, description: '组件路径' },
-    icon: { type: 'string', required: false, allowEmpty: true, description: 'icon' },
-    permission: { type: 'string', required: false, allowEmpty: true, description: '权限标识' },
+    name: { type: 'string', required: true, description: '栏目名称', message: '栏目名称不能为空' },
+    en_name: { type: 'string', required: false, allowEmpty: true, description: '英文栏目名称' },
+    code: { type: 'string', required: false, allowEmpty: true, description: '编码' },
+    route: { type: 'string', required: false, allowEmpty: true, description: '路由' },
+    type: { type: 'string', required: true, description: '类型' },
+    url: { type: 'string', required: false, allowEmpty: true, description: '外链' },
+    articleid: { type: 'string', required: false, allowEmpty: true, description: '单页关联文章id' },
+    image: { type: 'string', required: false, allowEmpty: true, description: '栏目图' },
+    description: { type: 'string', required: false, allowEmpty: true, description: '描述' },
+    meta_title: { type: 'string', required: false, allowEmpty: true, description: 'seo标题' },
+    meta_keywords: { type: 'string', required: false, allowEmpty: true, description: 'seo关键词' },
+    meta_description: { type: 'string', required: false, allowEmpty: true, description: 'seo描述' },
     sort: { type: 'number', required: false, allowEmpty: true, description: '排序' },
-    type: { type: 'number', required: false, allowEmpty: true, description: '类型' },
-    isLock: { type: 'boolean', required: false, allowEmpty: true, description: '是否锁定' },
-    cache: { type: 'boolean', required: false, allowEmpty: true, description: '缓存' },
-    redirect: { type: 'string', required: false, allowEmpty: true, description: '重定向' },
+    enable: { type: 'boolean', required: false, allowEmpty: true, description: '是否锁定' },
   },
 }
 ;
